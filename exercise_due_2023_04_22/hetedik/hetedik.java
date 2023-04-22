eedaf1d1046ffa48877f0a1de14e49af4d28dad6
peterkimpan <peterkimpan@gmail.com>
2023-04-22 13:04:37 +0200

    Done exercise 7 - Needs clarification

--- exercise_due_2023_04_22/hetedik/hetedik.java
+++ exercise_due_2023_04_22/hetedik/hetedik.java
@@ -1,5 +1,18 @@
 package exercise_due_2023_04_22.hetedik;
 
+import java.util.Scanner;
+
 public class hetedik {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kérem az órát: ");
+        int ora = input.nextInt();
+        if(ora<=12){
+            System.out.print("Currently it's "+ora+" o'clock in the morning.");
+        }
+        else{
+            System.out.print("Currently it's "+ora%12+" o'clock in the evening.");
+        }       
+        input.close();
+    }
 }
