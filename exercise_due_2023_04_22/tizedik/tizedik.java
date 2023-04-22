8a2a4118e79de3e0678bc6f9ca7ecdfc119a5134
peterkimpan <peterkimpan@gmail.com>
2023-04-22 13:16:58 +0200

    Done exercise 10

--- exercise_due_2023_04_22/tizedik/tizedik.java
+++ exercise_due_2023_04_22/tizedik/tizedik.java
@@ -1,5 +1,8 @@
 package exercise_due_2023_04_22.tizedik;
 
 public class tizedik {
-    
+    public static void main(String[] args) {
+        final double szam = 12.5;
+        System.out.println("A szám("+szam+") kétszerese: "+(szam*2)+", fele: "+(szam/2));
+    }
 }
