2da03459929ed1a63b30ac1144b92a7610412800
peterkimpan <peterkimpan@gmail.com>
2023-04-22 13:12:31 +0200

    Done exercise 9

--- exercise_due_2023_04_22/kilencedik/kilencedik.java
+++ exercise_due_2023_04_22/kilencedik/kilencedik.java
@@ -1,5 +1,19 @@
 package exercise_due_2023_04_22.kilencedik;
 
 public class kilencedik {
-    
+    public static void main(String[] args) {
+        byte largestByte = Byte.MAX_VALUE; 
+        short largestShort = Short.MAX_VALUE; 
+        int largestInteger = Integer.MAX_VALUE; 
+        long largestLong = Long.MAX_VALUE; 
+        float largestFloat = Float.MAX_VALUE; 
+        double largestDouble = Double.MAX_VALUE;
+        System.out.println("Max value for largest byte: "+largestByte);
+        System.out.println("Max value for largest short: "+largestShort);
+        System.out.println("Max value for largest int: "+largestInteger);
+        System.out.println("Max value for largest long: "+largestLong);
+        System.out.println("Max value for largest float: "+largestFloat);
+        System.out.println("Max value for largest double: "+largestDouble);
+
+    }
 }
