ac10dbfb8721b1f5d5faba7fb77db5d8ed34b086
peterkimpan <peterkimpan@gmail.com>
2023-04-22 12:46:15 +0200

    Done exercise 5

--- exercise_due_2023_04_22/otodik/otodik.java
+++ exercise_due_2023_04_22/otodik/otodik.java
@@ -1,5 +1,33 @@
 package exercise_due_2023_04_22.otodik;
 
+import java.util.Scanner;
+
 public class otodik {
-    
+    public static void main(String[] args) {
+        final String felhasznaloNev = "felhasznalo1";
+        final String felhasznaloJelszo = "jelszo123";
+        String nev = new String();
+        String jelszo = new String();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Felhasználónév: ");
+        nev=input.nextLine();
+        System.out.print("Jelszó: ");
+        jelszo=input.nextLine();
+        int probalkozasok = 0;
+        while(!(felhasznaloNev.equals(nev) & felhasznaloJelszo.equals(jelszo))  && probalkozasok < 2){
+            System.out.println("Rossz felhasználónév vagy jelszó!");
+            System.out.print("Felhasználónév: ");
+            nev=input.nextLine();
+            System.out.print("Jelszó: ");
+            jelszo=input.nextLine();
+            probalkozasok++;
+        }
+        if(probalkozasok < 3){
+            System.out.println("Üdvözöljük újra!");
+        }
+        else{
+            System.out.println("Sajnos túllépte a maximális próbálkozások számát. Próbálja újra 3 perc múlva!");
+        }
+        input.close();
+    }
 }
