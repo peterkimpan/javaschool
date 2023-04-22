b4aaa455daa15b566757f87e06b4eba40d5b6a7c
peterkimpan <peterkimpan@gmail.com>
2023-04-22 12:25:41 +0200

    Done exercise 4

--- exercise_due_2023_04_22/negyedik/negyedik.java
+++ exercise_due_2023_04_22/negyedik/negyedik.java
@@ -1,5 +1,9 @@
 package exercise_due_2023_04_22.negyedik;
 
 public class negyedik {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            if(i%7 == 0){System.out.print(i+" ");}
+        }
+    }
 }
