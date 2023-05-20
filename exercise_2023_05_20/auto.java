ab46f89ae46b38aff085f9c0689c734c5e76a037
peterkimpan <peterkimpan@gmail.com>
2023-05-20 09:49:16 +0200

    Variables set to private, added "setName" methods

--- exercise_2023_05_20/auto.java
+++ exercise_2023_05_20/auto.java
@@ -1,15 +1,27 @@
 package exercise_2023_05_20;
 
 public class auto{
-    String név;
-    String szín;
-    Integer alvázszám;
-    Integer kilóméteróra;
+    private String név;
+    private String szín;
+    private Integer alvázszám;
+    private Integer kilóméteróra;
     public void kilóméterórakiír(){
-        System.out.println("A(z) "+név+" autó kilóméterórája: "+kilóméteróra);
+        System.out.println("A(z) "+név+" autó kilóméterórája: "+kilóméteróra+" km");
     }
     public Integer hozzáadkilómétert(int mennyit){
         kilóméteróra = kilóméteróra + mennyit;
         return kilóméteróra;
+    }
+    public void setName(String name){
+        név = name;
+    }
+    public void setColor(String color){
+        szín = color;
+    }
+    public void setChassisNumber(int number){
+        alvázszám = number;
+    }
+    public void setKm(int km){
+        kilóméteróra = km;
     }
 }
--- exercise_2023_05_20/autok.java
+++ exercise_2023_05_20/autok.java
@@ -8,6 +8,9 @@ public class autok {
         //bmw = new auto(); így is lehet
         //mercedes = new auto();
         //porsche = new auto();
+
+        /*Régi módszer public változókkal */
+        /*
         bmw.név = "BMW X5";
         mercedes.név ="Mercedes ASD";
         porsche.név = "Porsche XYZ";
@@ -20,10 +23,23 @@ public class autok {
         bmw.kilóméteróra = 17755;
         mercedes.kilóméteróra = 44343;
         porsche.kilóméteróra = 44222;
-
+        */
+        bmw.setName("BMW X5");
+        mercedes.setName("Mercedes ASD");
+        porsche.setName("Porsche XYZ");
+        bmw.setColor("Piros");
+        mercedes.setColor("Kék");
+        porsche.setColor("Fehér");
+        bmw.setChassisNumber(21422142);
+        mercedes.setChassisNumber(44434333);
+        porsche.setChassisNumber(55453343);
+        bmw.setKm(17755);
+        mercedes.setKm(44343);
+        porsche.setKm(44222);
+        
         bmw.kilóméterórakiír();
         //Első módszer: A visszakapott értéket íratom ki println-al:
-        System.out.println("Az autó új kilóméteróraállása: "+bmw.hozzáadkilómétert(200));
+        System.out.println("Az autó új kilóméteróraállása: "+bmw.hozzáadkilómétert(200)+" km");
         //Második módszer: Növelem a kilóméterórát a "hozzáadkilómétert" metódussal, majd kiíratom a "kilóméterórakiír"-al
         bmw.hozzáadkilómétert(150);
         bmw.kilóméterórakiír();
