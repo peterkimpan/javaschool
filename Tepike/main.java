53cf535072b35cc16e7d84e7d16d750182ccef06
Tepliczki Adam <68915039+tepike@users.noreply.github.com>
2023-05-28 17:08:05 +0200

    Update main.java

--- Tepike/main.java
+++ Tepike/main.java
@@ -2,7 +2,22 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("Játék elindult");
+        System.out.println("Játék elindult\n");
         körök.Rounds();
+    
+
+
     }
 }
+
+
+/*
+\t – tab
+\b – backspace (a step backward in the text or deletion of a single character)
+\n – new line
+\r – carriage return
+\f – form feed
+\’ – single quote
+\” – double quote
+\\ – backslash
+ */
