ec946e3647afaf6dd67291fda4dc7994f0b5633e
Tepliczki Adam <68915039+tepike@users.noreply.github.com>
2023-05-28 17:07:13 +0200

    Update badguy.java

--- Tepike/badguy.java
+++ Tepike/badguy.java
@@ -1,4 +1,8 @@
+import java.util.Random;
 public class badguy {
+    Random random = new Random();
+    int randomNumber = random.nextInt(10) + 1;
+
 
     /*static kellet a név elé, hogy tudja kezelni  hero.Név/badguy.Név formában másik programkód */
     static String Név="Diablo";
@@ -6,6 +10,7 @@ public class badguy {
     String Heal;
     int Poison=10;
     int Damage=5;
+    int Mana=10;
 
 
 
@@ -20,17 +25,25 @@ public class badguy {
         Élet=Életmegadása;
     }
 
+    public void setBadguy_Mana(){
+        Mana=Mana+2;
+        if(Mana>10){
+            Mana=10;
+        }
+
+    }
+
+
 
     public void Mérgezés(){
         /*getKöröm még nem működik. Cél, hogy 2 körig mérgezzen és ne lehessen csak 3-4 körönként használni */
-        körök.getKöröm();
-        
-        int Hős_élet= hero.getÉlet();
-        int sebzett_élet= Hős_élet-Poison;
-        
-        hero.setÉlet(sebzett_élet);
-        
-        System.out.println("A hős életéből levett a méreg: "+Poison+"-et, így a fennmaradó mennyiség= "+sebzett_élet);
+        if (Mana>=5){
+            int Hős_élet= hero.getÉlet();
+            int sebzett_élet= Hős_élet-Poison;
+            hero.setÉlet(sebzett_élet);
+            System.out.println("A hős életéből levett a méreg: "+Poison+"-et, így a fennmaradó mennyiség = "+sebzett_élet);
+        }
+           
     }
 
     public void Mérgezés_nagysága(){
