927dfe55b492a1ebb843898676cc60a83cd6517c
Tepliczki Adam <68915039+tepike@users.noreply.github.com>
2023-05-28 17:07:32 +0200

    Update hero.java

--- Tepike/hero.java
+++ Tepike/hero.java
@@ -6,11 +6,12 @@ public class hero {
     int  Healmennyiség=5;
     int Damage=10;
     int Damage_boost=5;
+    int Mana=0;
+
     
     public String getHős_neve(){
         return Név;
     }
-
 
     public static int getÉlet(){
         return  Élet;
@@ -37,10 +38,28 @@ public class hero {
         Damage=a;
     }
 
+    public void setFőhős_Mana(){
+        Mana=Mana+2;
+        if(Mana>10){
+            Mana=10;
+        }
+
+    }
+
+    public void setFőhős_Mana_Használt(){
+        Mana=Mana-6;
+    }
+    public void setFőhős_Heal_Használt(){
+        Mana=Mana-5;
+    }
+
+
     public void Boost_damagebe(){
+        if(Mana>=6){
         setDamage(Damage);
         Damage=Damage+Damage_boost;
-        System.out.println("\nA hősünk ("+Név+")"+" növelt sebzési ereje: "+Damage);
+        System.out.println("\nA hősünk ("+Név+")"+" növelt támadási ereje: "+Damage+"-öt sebzett "+badguy.Név+"-n");
+        }
 
     }
 
