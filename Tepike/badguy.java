0d586ffc7d954fefc5c7636cd1ffc97d23fb0e49
Tepliczki Adam <tepi78989@gmail.com>
2023-05-29 18:14:24 +0200

    Javított_méreg_sorsolás

--- Tepike/badguy.java
+++ Tepike/badguy.java
@@ -1,14 +1,13 @@
 import java.util.Random;
 public class badguy {
     Random random = new Random();
-    int Random_Mérgezés = random.nextInt(12) + 1;
 
 
     /*static kellet a név elé, hogy tudja kezelni  hero.Név/badguy.Név formában másik programkód */
     static String Név="Diablo";
     private static int Élet=100;
     String Heal;
-    int Poison=10;
+    int Poison=0;
     int Damage=5;
     int Mana=10;
 
@@ -33,23 +32,21 @@ public class badguy {
 
     }
 
-    public void setMéreg(int Random_Mérgezés){
-        if (Random_Mérgezés<6)
-            Random_Mérgezés=6;
-        Poison=Random_Mérgezés;
-    }
 
 
 
     public void Mérgezés(){
         /*getKöröm még nem működik. Cél, hogy 2 körig mérgezzen és ne lehessen csak 3-4 körönként használni */
-        if (Mana>=5){
-            int Hős_élet= hero.getÉlet();
-            setMéreg(Random_Mérgezés);
-            int sebzett_élet= Hős_élet-Poison;
-            hero.setÉlet(sebzett_élet);
-            System.out.println("A hős életéből levett a méreg: "+Poison+"-et, így a fennmaradó mennyiség = "+sebzett_élet);
-        }
+        int Random_Mérgezés = random.nextInt(12);
+        int Hős_élet= hero.getÉlet();
+        if (Random_Mérgezés<6)
+            Random_Mérgezés=6;
+            Poison=Random_Mérgezés;
+        Poison=Random_Mérgezés;
+        int sebzett_élet= Hős_élet-Poison;
+        hero.setÉlet(sebzett_élet);
+        System.out.println("A hős életéből levett a méreg: "+Poison+"-et, így a fennmaradó mennyiség = "+sebzett_élet);
+        
            
     }
 
