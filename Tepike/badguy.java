4468eb5ac1924de5767179db058733a370485a9c
Tepliczki Adam <tepi78989@gmail.com>
2023-05-29 17:51:16 +0200

    Mérgezési_Nagyság_Sorsolása

--- Tepike/badguy.java
+++ Tepike/badguy.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 public class badguy {
     Random random = new Random();
-    int randomNumber = random.nextInt(10) + 1;
+    int Random_Mérgezés = random.nextInt(12) + 1;
 
 
     /*static kellet a név elé, hogy tudja kezelni  hero.Név/badguy.Név formában másik programkód */
@@ -33,12 +33,19 @@ public class badguy {
 
     }
 
+    public void setMéreg(int Random_Mérgezés){
+        if (Random_Mérgezés<6)
+            Random_Mérgezés=6;
+        Poison=Random_Mérgezés;
+    }
+
 
 
     public void Mérgezés(){
         /*getKöröm még nem működik. Cél, hogy 2 körig mérgezzen és ne lehessen csak 3-4 körönként használni */
         if (Mana>=5){
             int Hős_élet= hero.getÉlet();
+            setMéreg(Random_Mérgezés);
             int sebzett_élet= Hős_élet-Poison;
             hero.setÉlet(sebzett_élet);
             System.out.println("A hős életéből levett a méreg: "+Poison+"-et, így a fennmaradó mennyiség = "+sebzett_élet);
