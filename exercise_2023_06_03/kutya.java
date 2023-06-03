559776e8651cf2ffacd4f5e938666f1f059dc1a7
peterkimpan <peterkimpan@gmail.com>
2023-06-03 08:14:37 +0200

    Kutya típus létrehozása

---
+++ exercise_2023_06_03/kutya.java
@@ -0,0 +1,9 @@
+package exercise_2023_06_03;
+
+    public class kutya{
+    private String név;
+    private String fajta;
+    private Integer születési_idő;
+    private Integer neme;
+    private Integer chipszám;
+    }
